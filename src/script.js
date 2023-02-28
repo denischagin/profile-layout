d94b3cf73ab3cc25cad422fb23af50396e9e456f
denischagin <denischaginnn@gmail.com>
2023-02-28 22:03:02 +0300

    full site

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const menuItems = document.querySelectorAll(".menu__item a")
+
+menuItems.forEach((element, index) => {
+    element.addEventListener("click", e => {
+        e.preventDefault()
+
+        const id = element.getAttribute('href')
+        const elementToView = document.querySelector(id);
+
+        elementToView.scrollIntoView({
+            block: "start",
+            behavior: "smooth"
+        })
+    })
+})
