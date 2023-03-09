25a23f8d5f002d5f1ba2ed3c9e33975c32d5498e
denischagin <denischaginnn@gmail.com>
2023-03-10 00:57:30 +0300

    menu

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
-const menuItems = document.querySelectorAll(".menu__item a")
+// const menuItems = document.querySelectorAll(".menu__item a")
+const menuBurger = document.getElementById("menu-burger")
+const header = document.getElementById('header')
+const menuItems = document.querySelectorAll('.menu__item a')
+const pullOutMenu = document.getElementById('pull-out-menu')
+const buttonCloseMenu = document.getElementById('close-menu')
+const menuPullOutItems = document.querySelectorAll(".menu__item")
 
 menuItems.forEach((element, index) => {
     element.addEventListener("click", e => {
         e.preventDefault()
-
+        if (element.parentElement.classList.contains('active')) 
+            pullOutMenu.classList.remove('active')
         const id = element.getAttribute('href')
         const elementToView = document.querySelector(id);
 
@@ -13,3 +20,13 @@ menuItems.forEach((element, index) => {
         })
     })
 })
+
+menuBurger.addEventListener('click', e => {
+    e.preventDefault()
+    pullOutMenu.classList.toggle('active')
+})
+
+buttonCloseMenu.addEventListener('click', e => {
+    pullOutMenu.classList.remove('active')
+})
+
